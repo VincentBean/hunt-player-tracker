155a5ac885cea182a9cd295488d5e2b5a5ed0fb5
Vincent Boon <vincent@justbetter.nl>
2021-11-16 20:42:48 +0100

    Automatic deployment hook

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/deploy', function(Request $request) {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    $key = config('services.deploy_key');
+
+    if ($key === null) {
+        return response('No deployment key set', 500);
+    }
+
+    if ($key !== $request->key) {
+        abort(404);
+    }
+
+    exec("sh " . base_path() . "/deploy.sh &");
+
+    return response('deploying');
 });
