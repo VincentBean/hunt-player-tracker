9435f1f22041f4be51e142a4852e3bf77feeebc7
Vincent Boon <vincent@justbetter.nl>
2021-11-16 20:48:15 +0100

    Add optimize to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 php8.0 artisan down
+php8.0 artisan optimize:clear
 git pull origin main
 php8.0 /usr/local/bin/composer install
 php8.0 artisan migrate
 npm i
 npm run prod
+php8.0 artisan optimize
 php8.0 artisan up
