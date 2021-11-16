70669b7cda783374e42ea4092cf07a848bac463e
Vincent Boon <vincent@justbetter.nl>
2021-11-16 20:52:00 +0100

    testing webhook

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/deploy', function(Request $request) {
+Route::post('/deploy', function(Request $request) {
 
-    \Log::info($request->toArray());
+    $sig_check = 'sha1=' . hash_hmac('sha1', $request->getContent(), config('services.deploy_key'));
+
+    if ($sig_check !== $request->header('x-hub-signature')) {  // php >=5.6 and above should use hash_equals() for comparison
+        \Log::info('MATCH');
+    }
+    \Log::info('FAIL');
+
+
     return response('deploying');
     $key = config('services.deploy_key');
 
